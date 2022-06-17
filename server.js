ace070fda373e9b50287cb1b762df5aa091088e8
mistlet0e <48447115+mistlet0e@users.noreply.github.com>
2022-06-17 23:35:16 +0800

    ver1

---
+++ server.js
@@ -0,0 +1,28 @@
+/*set up static file server*/
+let static = require("node-static");
+
+/*set up http server*/
+let http = require("http");
+const { dir } = require("console");
+
+/*run on heroku*/
+let port = process.env.PORT;
+let directory = __dirname + "/public";
+
+if ((port === "undefined") || (port === null)){
+    port = 8080;
+    directory = "./public";
+}
+
+let file = new static.Server(directory);
+
+let app = http.createServer(
+    function(request,response){
+        request.addListener("end",
+        function(){
+            file.serve(request,response);
+        }).resume();
+    }
+).listen(port);
+
+console.log("the server is running");
